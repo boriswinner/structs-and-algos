a92dfe713ebb4d78f5431efa7d10af8bf3ee05e6
Boris Timofeenko <timbori@mail.ru>
2017-05-08 16:30:25 +1000

    Update binary heap.c

--- binary heap.c
+++ binary heap.c
@@ -2,9 +2,11 @@
 #include <malloc.h>
 #include <stdbool.h>
 
-#define MAXHEAPSIZE 100
+#define MAXHEAPSIZE 1000000
 #define MAXHEAP 1
 #define MINHEAP 0
+
+#define SWAP(a, b) do { typeof(a) tmp = a; a = b; b = tmp; } while (0)
 
 typedef struct heap{
     int HeapSize;
@@ -12,46 +14,81 @@ typedef struct heap{
     bool mode;
 } heap;
 
-void initializeHeap(heap* aheap, bool amode){
+void heapInitialize(heap* aheap, bool amode){
     aheap->h = malloc(MAXHEAPSIZE*sizeof(int));
     aheap->HeapSize = 0;
     aheap->mode = amode;
 }
 
-void add(int n, heap* aheap){
-    int i = aheap->HeapSize, p = (i-1)/2;
-    aheap->h[i] = n;
-    while ((p >= 0) && (i > 0)){
-        if ((aheap->h[i] > aheap->h[p]) && (aheap->mode)){
-            int t = aheap->h[i];
-            aheap->h[i] = aheap->h[p];
-            aheap->h[p] = t;
-        }
-        i = p;
-        p = (i-1)/2;
+void heapSiftUp(heap* aheap, int i){
+    while(aheap->mode ? (aheap->h[i] > aheap->h[(i-1)/2]) : (aheap->h[i] < aheap->h[(i-1)/2])){
+        SWAP(aheap->h[i], aheap->h[(i-1)/2]);
+        i = (i-1)/2;
     }
-    aheap->HeapSize++;
 }
 
-void heapArrsyPrint(heap* aheap){
+void heapSiftDown(heap* aheap, int i){
+    int l,r,j;
+    while (2*i + 1 < aheap->HeapSize){
+        l = 2*i + 1;
+        r = 2*i + 2;
+        j = l;
+        if ((r < aheap->HeapSize) && (aheap->mode ? (aheap->h[r] > aheap->h[l]) : (aheap->h[r] < aheap->h[l]))){
+            j = r;
+        }
+        if (aheap-> mode ? aheap->h[i] >= aheap->h[j] : aheap->h[i] <= aheap->h[j]){
+            break;
+        }
+        SWAP(aheap->h[i], aheap->h[j]);
+        i = j;
+    }
+}
+
+void heapAdd(int n, heap* aheap){
+    aheap->HeapSize++;
+    aheap->h[aheap->HeapSize-1] = n;
+    heapSiftUp(aheap,aheap->HeapSize-1);
+}
+
+int heapDeleteRoot(heap* aheap){
+    int root = aheap->h[0];
+    aheap->h[0] = aheap->h[aheap->HeapSize - 1];
+    aheap->HeapSize--;
+    heapSiftDown(aheap,0);
+    return (root);
+}
+
+int heapGetRoot(heap* aheap){
+    return(aheap->h[0]);
+}
+
+void heapArrayPrint(heap* aheap){
     for (int i = 0; i < aheap->HeapSize; ++i){
         printf("%d ",aheap->h[i]);
     }
 }
 
+void heapPrint(heap* aheap){
+    int i = 0, k = 1;
+    while (i < aheap->HeapSize){
+        while ((i < aheap->HeapSize) && (i < k)){
+            printf("%d ",aheap->h[i]);
+            ++i;
+        }
+        printf("\n");
+        k = k*2 + 1;
+    }
+}
+
 int main() {
-    heap testheap;
-    initializeHeap(&testheap,MAXHEAP);
-    add(11,&testheap);
-    add(9,&testheap);
-    add(5,&testheap);
-    add(8,&testheap);
-    add(6,&testheap);
-    add(16,&testheap);
-    add(1,&testheap);
-    add(4,&testheap);
-    add(2,&testheap);
-    heapArrsyPrint(&testheap);
-    printf("Hello, World!\n");
+    heap myheap;
+    heapInitialize(&myheap,MAXHEAP);
+    heapAdd(3,&myheap);
+    heapAdd(1,&myheap);
+    heapAdd(15,&myheap);
+    heapAdd(4,&myheap);
+    heapAdd(-5,&myheap);
+    heapDeleteRoot(&myheap);
+    heapPrint(&myheap);
     return 0;
 }
